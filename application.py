8d1477078ce0de9982564d8dee5cff95bc780de3
rishabh kumar mishra <rishabhmishra03606@gmail.com>
2025-10-11 14:27:33 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
 from flask import Flask,request,render_template
 import numpy as np
-
 import pandas as pd
+from sklearn.preprocessing import StandardScale
 
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 import os
 import sys
